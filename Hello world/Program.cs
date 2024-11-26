e383c6cff1a8944ae6c324949fb25325bcc50d18
Tanatip2005 <tanatipkuan@gmail.com>
2024-11-26 11:27:46 +0700

    tanatip

---
+++ Hello world/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+string name = "Tanatip Kuansut";
+string student = "673450194-6";
+// string is datatype -> ชนิดของข้อมูล
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("my student id:" + student);
